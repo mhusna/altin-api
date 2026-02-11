13d72c3792c4cb8b371eb029a7bb211348fe7c94
mhusna <mehmethusna@outlook.com>
2026-02-12 00:13:45 +0300

    axios ile istek atıldı

--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
 // Use "type: module" in package.json to use ES modules
 import express from "express";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 app.use(cors());
- 
+
+const api_key = "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";
 // Define your routes
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
+app.get('/', async (request, response) => {
+
+  const res = await axios.get("https://hedef-api.com/endpoint", {
+    headers: {
+      "Authorization": `Bearer ${api_key}`
+    }
+  });
+
+  console.log(res.status);
+  console.log(res.data);
+  response.json({ data: res.data });
 });
  
 // Export the Express app
